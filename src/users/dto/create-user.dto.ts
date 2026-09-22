9327d423da3a9a55fbe63f99d0939189faee2ac4
Nitiema Allassane <allassanenitiema@gmail.com>
2026-09-22 10:53:28 +0000

    add: create, find & findOne api route

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { 
+    IsEmail, 
+    IsNotEmpty, 
+    IsString,
+    Length,
+    Matches,
+    MinLength
+} from 'class-validator'
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    fullName: string;
+
+    
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @Matches(/^[0-9]+$/, { message: 'Le numéro ne doit contenir que des chiffres' })
+    @Length(10, 10)
+    phone: string;
+
+    @IsString()
+    @MinLength(8)
+    password: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from '../prisma/prisma.service.js';
+
+
+const userSelect = {
+  id: true,
+  fullName: true,
+  phone: true,
+  email: true,
+  createdAt: true,
+  updatedAt: true,
+} as const;
+
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10)
+    return this.prisma.user.create({
+      data: {
+        password: hashedPassword,
+        fullName: createUserDto.fullName,
+        phone: createUserDto.phone,
+        email: createUserDto.email
+      }, 
+      select: userSelect
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return this.prisma.user.findMany({
+      select: userSelect
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    const user = await this.prisma.user.findUnique({
+      select: userSelect,
+      where: { id }
+    });
+
+    if (!user) {
+      throw new NotFoundException(`Utilisateur avec l'id ${id} introuvable`);
+    }
+
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
