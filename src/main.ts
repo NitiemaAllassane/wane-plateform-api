b64e735c3d494ad55d70bde94f22e3086c01a1d9
Nitiema Allassane <allassanenitiema@gmail.com>
2026-09-23 11:03:42 +0000

    (config): configuration du CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "http://localhost:3001",
+    Credential: true
+  })
   app.useGlobalPipes(new ValidationPipe())
+
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
